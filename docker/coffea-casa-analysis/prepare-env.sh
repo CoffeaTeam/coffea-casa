980d35e1b7ee20b403d0f5eacedf6ad1b2515e7a
Oksana Shadura <oksana.shadura@cern.ch>
2020-05-27 08:11:22 +0200

    Patch by bbockelman

--- docker/coffea-casa-analysis/prepare-env.sh
+++ docker/coffea-casa-analysis/prepare-env.sh
@@ -11,11 +11,16 @@ if [ -f "/etc/cmsaf-secrets/xcache_token" ]; then
     export BEARER_TOKEN_FILE="/etc/cmsaf-secrets/xcache_token"
 fi
 
+sleep 10
+
 #HTCondor port and hostname
 PORT=`cat $_CONDOR_JOB_AD | grep HostPort | tr -d '"' | awk '{print $NF;}'`
 HOST=`cat $_CONDOR_JOB_AD | grep RemoteHost | tr -d '"' | tr '@' ' ' | awk '{print $NF;}'`
+NAME=`cat $_CONDOR_JOB_AD | grep DaskWorkerName | tr -d '"' | awk '{print $NF;}'`
 
 # for now hardcoded ( --nprocs 1)
-HTCONDOR_COMAND="/opt/conda/bin/python -m distributed.cli.dask_worker 129.93.183.33:8787 --nthreads 4 --memory-limit 2000.00MB --name 0 --nanny --death-timeout 60"
-# --contact-address tcp://$HOST:$PORT
-exec $HTCONDOR_COMAND --contact-address tcp://$HOST:$PORT --listen-address tcp://0.0.0.0:8787 
+HTCONDOR_COMAND="/opt/conda/bin/python -m distributed.cli.dask_worker 129.93.183.33:8787 --name $NAME --nthreads 4  --memory-limit 2000.00MB --name 0 --nanny --death-timeout 60"
+echo "Copy of the job ClassAd:" 1>&2
+cat $_CONDOR_JOB_AD 1>&2
+echo $HTCONDOR_COMAND --contact-address tcp://$HOST:$PORT --listen-address tcp://0.0.0.0:8787 1>&2
+exec $HTCONDOR_COMAND --contact-address tcp://$HOST:$PORT --listen-address tcp://0.0.0.0:8787
