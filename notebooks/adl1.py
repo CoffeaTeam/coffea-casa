f7bc2e7c9246b31fe08acb53940f683c00fef790
Oksana Shadura <oksana.shadura@cern.ch>
2020-07-02 18:31:43 +0200

    Update tutorials with new CoffeaCasaCluster method (moving to subclass later)

--- notebooks/adl1.py
+++ notebooks/adl1.py
@@ -10,6 +10,8 @@ from dask.distributed import Client, LocalCluster
 from dask_jobqueue import HTCondorCluster
 from dask_jobqueue.htcondor import HTCondorJob
 
+from coffea_casa.coffea_casa import CoffeaCasaCluster
+
 fileset = {
     'Jets': { 'files': ['root://eospublic.cern.ch//eos/root-eos/benchmark/Run2012B_SingleMu.root'],
              'treename': 'Events'
@@ -53,42 +55,12 @@ class METProcessor(processor.ProcessorABC):
     def postprocess(self, accumulator):
         return accumulator
     
-    
-sec_dask = Security(tls_ca_file='/etc/cmsaf-secrets/ca.pem',
-               tls_worker_cert='/etc/cmsaf-secrets/hostcert.pem',
-               tls_worker_key='/etc/cmsaf-secrets/hostcert.pem',
-               tls_client_cert='/etc/cmsaf-secrets/hostcert.pem',
-               tls_client_key='/etc/cmsaf-secrets/hostcert.pem',
-               tls_scheduler_cert='/etc/cmsaf-secrets/hostcert.pem',
-               tls_scheduler_key='/etc/cmsaf-secrets/hostcert.pem',
-               require_encryption=True)
 
 HTCondorJob.submit_command = "condor_submit -spool"
 
-cluster = HTCondorCluster(cores=4,
-                          memory="2GB",
-                          disk="1GB",
-                          log_directory="logs",
-                          silence_logs="DEBUG",
-                          scheduler_options= {"dashboard_address":"8786", "port":8787, "external_address": "129.93.183.33:8787"},
-                          # HTCondor submit script
-                          job_extra={"universe": "docker",
-                                     # To be used with coffea-casa:0.1.11
-                                     "encrypt_input_files": "/etc/cmsaf-secrets/xcache_token",
-                                     #"docker_network_type": "host",
-                                     "docker_image": "coffeateam/coffea-casa-analysis:0.1.26", 
-                                     "container_service_names": "dask",
-                                     "dask_container_port": "8787",
-                                     "should_transfer_files": "YES",
-                                     "when_to_transfer_output": "ON_EXIT",
-                                     "+DaskSchedulerAddress": '"129.93.183.33:8787"',
-                                    })
+host_ip = os.getenv("HOST_IP")
 
-cluster.adapt(minimum_jobs=5, maximum_jobs=10)  # auto-scale between 5 and 10 jobs (interval=500, wait_count=1, target_duration=10 )
-
-client = Client(cluster)
-
-dask.config.config
+client = CoffeaCasaCluster(worker_image="coffeateam/coffea-casa-analysis:0.1.26", external_ip=host_ip)
 
 exe_args = {
         'client': client,
