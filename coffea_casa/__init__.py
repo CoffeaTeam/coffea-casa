64192454e19492af24fe6cbb715e3c4b5cc6a539
Oksana Shadura <oksana.shadura@cern.ch>
2020-07-13 16:45:50 +0200

    Revert back to CoffeaCasaCluster method (safe option for demo)

--- coffea_casa/__init__.py
+++ coffea_casa/__init__.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 
 from . import version
-from .coffea_casa import CoffeaCasaCluster
+#from .coffea_casa import CoffeaCasaCluster
+from .coffea_casa_method import CoffeaCasaCluster
 
 __version__ = version.__version__
