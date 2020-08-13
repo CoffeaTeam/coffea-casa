12e10f47a1ee962b99d1a4ba8a90e7edad992584
Oksana Shadura <oksana.shadura@cern.ch>
2020-08-13 18:05:40 +0200

    Move tests outside ot coffea_casa (no intention to add them as a part of distribution)

---
+++ tests/test_coffeacasa.py
@@ -0,0 +1,117 @@
+import os
+import sys
+import socket
+import pytest
+from distributed.security import Security
+from distributed import Client
+from coffea_casa import CoffeaCasaCluster
+
+os.environ["HOST_IP"] = socket.gethostbyname(socket.gethostname())
+
+
+def test_header():
+    with CoffeaCasaCluster(cores=1,
+                           memory="100MB",
+                           disk="100MB",
+                           worker_image="coffeateam/coffea-casa:latest"
+                           ) as cluster:
+        job_script = cluster.job_script()
+        print("HTCondor Job script:", job_script)
+        print("Scheduler specs:", cluster.scheduler_spec)
+        kwargs = CoffeaCasaCluster._modify_job_kwargs({})
+        print("CoffeaCasaCluster arguments:", kwargs)
+        assert cluster._dummy_job.job_header_dict["MY.DaskWorkerCores"] == 1
+        assert cluster._dummy_job.job_header_dict["MY.DaskWorkerDisk"] == 100000000
+        assert cluster._dummy_job.job_header_dict["MY.DaskWorkerMemory"] == 100000000
+
+
+def test_job_script():
+    with CoffeaCasaCluster(cores=4,
+                           processes=4,
+                           memory="500MB",
+                           disk="500MB",
+                           worker_image="coffeateam/coffea-casa:latest",
+                           env_extra=['export LANG="en_US.utf8"',
+                                      'export LC_ALL="en_US.utf8"'],
+                           job_extra={"+Extra": "True"},
+                           ) as cluster:
+        job_script = cluster.job_script()
+        print("HTCondor Job script:", job_script)
+        print("Scheduler specs:", cluster.scheduler_spec)
+        assert "RequestCpus = MY.DaskWorkerCores" in job_script
+        assert "RequestDisk = floor(MY.DaskWorkerDisk / 1024)" in job_script
+        assert "RequestMemory = floor(MY.DaskWorkerMemory / 1048576)" in job_script
+        assert "MY.DaskWorkerCores = 4" in job_script
+        assert "MY.DaskWorkerDisk = 500000000" in job_script
+        assert "MY.DaskWorkerMemory = 500000000" in job_script
+        assert 'MY.JobId = "$(ClusterId).$(ProcId)"' in job_script
+        assert "LANG=en_US.utf8" in job_script
+        assert "LC_ALL=en_US.utf8" in job_script
+        assert "export" not in job_script
+        assert "+Extra = True" in job_script
+        assert (
+            "{} -m distributed.cli.dask_worker tcp://".format(sys.executable)
+            in job_script
+        )
+        assert "--memory-limit 125.00MB" in job_script
+        assert "--nthreads 1" in job_script
+        assert "--nprocs 4" in job_script
+
+
+def test_scheduler():
+    with CoffeaCasaCluster(cores=1,
+                           memory="100MB",
+                           disk="100MB",
+                           worker_image="coffeateam/coffea-casa:latest",
+                           scheduler_options={
+                               "dashboard_address": 8786,
+                               "port": 8788}
+                           ) as cluster:
+        job_script = cluster.job_script()
+        print("HTCondor Job script:", job_script)
+        print("Scheduler specs:", cluster.scheduler_spec)
+        kwargs = CoffeaCasaCluster._modify_job_kwargs({})
+        print("CoffeaCasaCluster arguments:", kwargs)
+        #assert cluster.scheduler_spec["port"] == 8788
+        expected = os.environ["HOST_IP"]
+        assert expected in str(cluster.scheduler_spec)
+        #cluster.scale(1)
+        #assert expected in str(cluster.worker_spec)
+
+
+def test_security():
+    dirname = os.path.dirname(__file__)
+    key = os.path.join(dirname, "key.pem")
+    cert = os.path.join(dirname, "ca.pem")
+    security = Security(
+        tls_ca_file=cert,
+        tls_scheduler_key=key,
+        tls_scheduler_cert=cert,
+        tls_worker_key=key,
+        tls_worker_cert=cert,
+        tls_client_key=key,
+        tls_client_cert=cert,
+        require_encryption=True,
+    )
+
+    with CoffeaCasaCluster(cores=1,
+                           memory="100MB",
+                           disk="100MB",
+                           worker_image="coffeateam/coffea-casa:latest",
+                           security=security) as cluster:
+        job_script = cluster.job_script()
+        print("HTCondor JobAd script:", job_script)
+        print("Scheduler specs:", cluster.scheduler_spec)
+        kwargs = CoffeaCasaCluster._modify_job_kwargs({})
+        print("CoffeaCasaCluster arguments:", kwargs)
+        assert cluster.security == 'mioa'
+        assert cluster.security == security
+        assert cluster.scheduler_spec["options"]["security"] == security
+        assert "--tls-key {}".format(key) in job_script
+        assert "--tls-cert {}".format(cert) in job_script
+        assert "--tls-ca-file {}".format(cert) in job_script
+        #cluster.scale(jobs=1)
+        #with Client(cluster, security=security) as client:
+        #    future = client.submit(lambda x: x + 1, 10)
+        #    result = future.result()
+        #    assert result == 11
---
+++ tests/test_coffeacasa_args.py
@@ -0,0 +1,48 @@
+import pytest
+import os
+from pathlib import Path
+from coffea_casa import CoffeaCasaCluster
+from distributed.security import Security
+
+# Security settings for coffea-casa Dask scheduler
+SECRETS_DIR = Path('/etc/cmsaf-secrets')
+CA_FILE = SECRETS_DIR / "ca.pem"
+CERT_FILE = SECRETS_DIR / "hostcert.pem"
+
+@pytest.fixture
+def default_modified_kwargs():
+    return CoffeaCasaCluster._modify_job_kwargs({})
+
+
+def test_default_dask_container_port(default_modified_kwargs):
+    assert default_modified_kwargs["job_extra"]["dask_container_port"] == 8787
+
+
+def test_can_override_dask_container_port():
+    kwargs = CoffeaCasaCluster._modify_job_kwargs(dict(job_extra={"dask_container_port": 8788}))
+    assert kwargs["job_extra"]["dask_container_port"] == 8788
+
+
+def test_default_protocol(default_modified_kwargs):
+    if CA_FILE.is_file() and CERT_FILE.is_file():
+        assert default_modified_kwargs["scheduler_options"]["protocol"] == 'tls'
+    else:
+        assert default_modified_kwargs["scheduler_options"]["protocol"] == 'tcp'
+
+
+def test_can_override_security():
+    dirname = os.path.dirname(__file__)
+    key = os.path.join(dirname, "key.pem")
+    cert = os.path.join(dirname, "ca.pem")
+    security = Security(
+        tls_ca_file=cert,
+        tls_scheduler_key=key,
+        tls_scheduler_cert=cert,
+        tls_worker_key=key,
+        tls_worker_cert=cert,
+        tls_client_key=key,
+        tls_client_cert=cert,
+        require_encryption=True)
+    kwargs = CoffeaCasaCluster._modify_job_kwargs(dict(security=security))
+    assert kwargs["security"] == security
+    assert kwargs["scheduler_options"]["protocol"] == 'tls'
