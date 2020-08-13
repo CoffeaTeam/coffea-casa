0fb5443968b542d8c06f33af6dc7c9d06ad5c472
Oksana Shadura <oksana.shadura@cern.ch>
2020-08-13 18:29:53 +0200

    Add versioneer to track versions

--- coffea_casa/__init__.py
+++ coffea_casa/__init__.py
@@ -6,9 +6,16 @@ import yaml
 import os
 from .coffea_casa import CoffeaCasaCluster
 from .coffea_casa_method import coffea_casa_cluster
-from .version import __version__
 from . import config
 
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
+
+
+def is_number(in_value):
+    try:
+        float(in_value)
+        return True
+    except ValueError:
+        return False
