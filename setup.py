0836bf3e52a42db8e2686df87fb3e1620a0636b1
Oksana Shadura <oksana.shadura@cern.ch>
2020-08-13 17:58:54 +0200

    Add setup.py and configuration files for coffea_casa module

--- setup.py
+++ setup.py
@@ -1,25 +1,38 @@
+"""Setup.py for coffea_casa
+"""
 try:
     from setuptools import setup, find_packages
 except ImportError:
     from distutils.core import setup, find_packages
+import io
 
 INSTALL_REQUIRES = ['distributed',
                     'dask-jobqueue',
                     ]
 EXTRAS_REQUIRE = {}
 
+with io.open("README.md", "r", encoding="utf-8") as f:
+    readme = f.read()
+
+about = {}
+with io.open("coffea_casa/version.py", "r", encoding="utf-8") as f:
+    exec(f.read(), about)
+
 setup(name='coffea_casa',
-      version='0.0.1',
+      version=about["__version__"],
       packages=find_packages(),
       description='A Prototype U.S. CMS analysis facility',
+      long_description=readme,
       url='http://github.com/CoffeaTeam/coffea-casa.git',
       author='UNL',
       author_email='oksana.shadura@cern.ch',
       license='BSD-3-Clause License',
       install_requires=INSTALL_REQUIRES,
       extras_require=EXTRAS_REQUIRE,
+      include_package_data=True,
+      zip_safe=False,
       setup_requires=["pytest-runner", "flake8"],
-      tests_require=["pytest"],
+      tests_require=["pytest", 'htcondor'],
       classifiers=[
           "Development Status :: 4 - Beta",
           "Intended Audience :: Developers",
