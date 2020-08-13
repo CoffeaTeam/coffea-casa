c9957e23858377d774f798b81b6073b305480336
Oksana Shadura <oksana.shadura@cern.ch>
2020-08-13 18:05:40 +0200

    Move tests outside ot coffea_casa (no intention to add them as a part of distribution)

---
+++ tests/test_config.py
@@ -0,0 +1,7 @@
+import dask
+import pytest
+import coffea_casa.config
+
+
+def test_dask_config():
+    assert dask.config.get('jobqueue.coffea-casa.worker-image') == "coffeateam/coffea-casa:latest"
