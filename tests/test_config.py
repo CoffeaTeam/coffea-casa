2b99f23dffac972982e8ba256d6d2795e1d9a7c9
Oksana Shadura <oksana.shadura@cern.ch>
2020-12-09 17:26:38 +0100

    Update coffea version and bump docker image version

--- tests/test_config.py
+++ tests/test_config.py
@@ -4,4 +4,4 @@ import coffea_casa.config
 
 
 def test_dask_config():
-    assert dask.config.get('jobqueue.coffea-casa.worker-image') == "coffeateam/coffea-casa-analysis:0.2.37"
+    assert dask.config.get('jobqueue.coffea-casa.worker-image') == "coffeateam/coffea-casa-analysis:0.2.38"
