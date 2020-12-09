8bcbe2269db985f414dba95da5032a9dae67a67b
Oksana Shadura <oksana.shadura@cern.ch>
2020-12-09 16:29:56 +0100

    [test] Add servicex token generation

--- docker/coffea-casa/prepare-env.sh
+++ docker/coffea-casa/prepare-env.sh
@@ -2,6 +2,29 @@
 
 set -x
 
+# servicex token generation
+if [[ -f "/etc/cmsaf-secrets/.servicex" ]]; then
+    export condor_token=$(</etc/cmsaf-secrets/servicex_token)
+
+    echo "
+api_endpoints:
+  - endpoint: https://uproot.servicex.coffea.casa
+    token:
+    type: uproot
+    " > $HOME/.servicex
+
+    python -c '
+import yaml, os
+servicex=os.environ.get("HOME")+"/.servicex"
+with open(servicex) as f:
+    list_yaml=yaml.load(f,Loader=yaml.Loader)
+list_yaml["api_endpoints"][0]["token"] = os.environ.get("condor_token")
+with open(servicex, "w") as f:
+    yaml.dump(list_yaml,f)'
+
+    unset condor_token
+fi
+
 # We start by adding extra apt packages, since pip modules may required library
 if [ "$EXTRA_APT_PACKAGES" ]; then
     echo "EXTRA_APT_PACKAGES environment variable found.  Installing."
