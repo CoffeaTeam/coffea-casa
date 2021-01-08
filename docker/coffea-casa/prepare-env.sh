b52b0ee071f2c1cc24b9e1d7dc4064812e869be3
Oksana Shadura <oksana.shadura@cern.ch>
2021-01-08 12:51:31 +0100

    Fix servicex file generation, missing yaml and bump a version

--- docker/coffea-casa/prepare-env.sh
+++ docker/coffea-casa/prepare-env.sh
@@ -7,14 +7,17 @@ if [[ -f "/etc/cmsaf-secrets/.servicex" ]]; then
     export servicex_token=$(</etc/cmsaf-secrets/.servicex)
     # debug: it work locally in Hub, let's try to touch file
     touch /home/jovyan/.servicex
+    echo "Tokens: generating ServiceX config file."
+    # We are using second test instance to test JWT integration:
+    # replace later with https://uproot.servicex.coffea.casa
     echo "
 api_endpoints:
-  - endpoint: https://uproot.servicex.coffea.casa
+  - endpoint: https://uproot-jwt.servicex.coffea.casa
     token:
     type: uproot
     " > /home/jovyan/.servicex
 
-    python -c '
+    /opt/conda/bin/python -c '
 import yaml, os
 servicex="/home/jovyan/.servicex"
 with open(servicex) as f:
@@ -23,30 +26,31 @@ list_yaml["api_endpoints"][0]["token"] = os.environ.get("servicex_token")
 with open(servicex, "w") as f:
     yaml.dump(list_yaml,f)'
 
+    echo "Tokens: ServiceX config file was succesfully generated."
     unset condor_token
 fi
 
 # We start by adding extra apt packages, since pip modules may required library
 if [ "$EXTRA_APT_PACKAGES" ]; then
-    echo "EXTRA_APT_PACKAGES environment variable found.  Installing."
+    echo "apt: EXTRA_APT_PACKAGES environment variable found.  Installing."
     apt update -y
     apt install -y $EXTRA_APT_PACKAGES
 fi
 
-if [ -e "/opt/app/environment.yml" ]; then
-    echo "environment.yml found. Installing packages"
+if [ -e "$HOME/environment.yml" ]; then
+    echo "conda: environment.yml found. Installing packages"
     /opt/conda/bin/conda env update -f /opt/app/environment.yml
 else
     echo "no environment.yml"
 fi
 
 if [ "$EXTRA_CONDA_PACKAGES" ]; then
-    echo "EXTRA_CONDA_PACKAGES environment variable found.  Installing."
+    echo "conda: EXTRA_CONDA_PACKAGES environment variable found.  Installing."
     /opt/conda/bin/conda install -y $EXTRA_CONDA_PACKAGES
 fi
 
 if [ "$EXTRA_PIP_PACKAGES" ]; then
-    echo "EXTRA_PIP_PACKAGES environment variable found.  Installing".
+    echo "pip: EXTRA_PIP_PACKAGES environment variable found.  Installing".
     /opt/conda/bin/pip install $EXTRA_PIP_PACKAGES
 fi
 
--- tests/test_config.py
+++ tests/test_config.py
@@ -4,4 +4,4 @@ import coffea_casa.config
 
 
 def test_dask_config():
-    assert dask.config.get('jobqueue.coffea-casa.worker-image') == "coffeateam/coffea-casa-analysis:0.3.6"
+    assert dask.config.get('jobqueue.coffea-casa.worker-image') == "coffeateam/coffea-casa-analysis:0.3.7"
