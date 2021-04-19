fe10f13dae4d0b3380dbb0178bceaac10c3a743c
Oksana Shadura <ksu.shadura@gmail.com>
2021-04-19 13:50:08 +0200

    Upgrade worker version to the recent one

--- tests/test_config.py
+++ tests/test_config.py
@@ -4,4 +4,4 @@ import coffea_casa.config
 
 
 def test_dask_config():
-    assert dask.config.get('jobqueue.coffea-casa.worker-image') == "coffeateam/coffea-casa-analysis:2021.04.17"
+    assert dask.config.get('jobqueue.coffea-casa.worker-image') == "coffeateam/coffea-casa-analysis:2021.04.19"
