fa9e316305b4613264c01b3c3dc3d70ec374ff0c
Oksana Shadura <ksu.shadura@gmail.com>
2021-05-04 13:46:16 +0200

    Update image to post-tag instead dev (coffea-casa module is still installed from post tag)

--- tests/test_config.py
+++ tests/test_config.py
@@ -4,4 +4,4 @@ import coffea_casa.config
 
 
 def test_dask_config():
-    assert dask.config.get('jobqueue.coffea-casa.worker-image') == "coffeateam/coffea-casa-analysis:2021.05.04dev"
+    assert dask.config.get('jobqueue.coffea-casa.worker-image') == "coffeateam/coffea-casa-analysis:2021.05.04post1"
