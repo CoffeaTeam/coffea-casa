a8f8349a2d18037c5caa1b980ed9b8811b5b3c46
Oksana Shadura <ksu.shadura@gmail.com>
2021-06-07 16:32:49 +0200

    Tag 2021.06.07 version

--- tests/test_config.py
+++ tests/test_config.py
@@ -4,4 +4,4 @@ import coffea_casa.config
 
 
 def test_dask_config():
-    assert dask.config.get('jobqueue.coffea-casa.worker-image') == "coffeateam/coffea-casa-analysis:2021.05.31"
+    assert dask.config.get('jobqueue.coffea-casa.worker-image') == "coffeateam/coffea-casa-analysis:2021.06.07"
