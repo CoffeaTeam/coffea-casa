476577b7d3c2532778ba10385ffd9640d89767dd
Oksana Shadura <ksu.shadura@gmail.com>
2021-07-06 17:37:32 +0200

    Update tag to 2021.07.06post0: coffea 0.7.5 and servicex token generation

--- docker/coffea-casa/prepare-env.sh
+++ docker/coffea-casa/prepare-env.sh
@@ -4,29 +4,14 @@ set -x
 
 export PYTHONPATH="$HOME/.local/lib/python3.8/site-packages:$PYTHONPATH"
 
-# ServiceX token generation
-if [[ -f "$SERVICEX_TOKEN" ]] && [[ "$SERVICEX_HOST" ]]; then
-    export servicex_token=$(<$SERVICEX_TOKEN)
-    touch /home/$NB_USER/.servicex
+if [[ "$SERVICEX_HOST" ]]; then
+    touch $HOME/.servicex
     echo "Tokens: generating ServiceX config file (available only from Jupyterhub notebook)"
     echo "
 api_endpoints:
   - endpoint: $SERVICEX_HOST
     type: uproot
     " > $HOME/.servicex
-
-### No need to add tokens until now (when revert please add "token:" in .servicex template)
-#    /opt/conda/bin/python -c '
-#import yaml, os
-#servicex="/home/$NB_USER/.servicex"
-#with open(servicex) as f:
-#    list_yaml=yaml.load(f,Loader=yaml.Loader)
-#list_yaml["api_endpoints"][0]["token"] = os.environ.get("servicex_token")
-#with open(servicex, "w") as f:
-#    yaml.dump(list_yaml,f)'
-
-#    echo "Tokens: ServiceX config file was succesfully generated."
-#    unset servicex_token
 fi
 
 if [ -e "$HOME/environment.yml" ]; then
