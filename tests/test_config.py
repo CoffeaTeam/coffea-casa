94b313885d9701585b7cea3cabe728f064299e84
Oksana Shadura <ksu.shadura@gmail.com>
2021-07-22 10:51:49 +0200

    Add new tag 2021.07.22 and check if tests are passing

--- tests/test_config.py
+++ tests/test_config.py
@@ -2,6 +2,9 @@ import dask
 import pytest
 import coffea_casa.config
 
+import os    
+os.environ['TAG'] = '2021.07.22'
+
 
 def test_dask_config():
     assert dask.config.get('jobqueue.coffea-casa.worker-image') == "coffeateam/coffea-casa-analysis:2021.07.13"
