5707f10b5e9fa115a623e74799612e5de7e290cf
Oksana Shadura <ksu.shadura@gmail.com>
2021-07-22 11:23:14 +0200

    Simplify coffea_casa module settings, add a list of TODOs

--- tests/test_config.py
+++ tests/test_config.py
@@ -2,9 +2,6 @@ import dask
 import pytest
 import coffea_casa.config
 
-import os    
-os.environ['TAG'] = '2021.07.22'
-
 
 def test_dask_config():
-    assert dask.config.get('jobqueue.coffea-casa.worker-image') == "coffeateam/coffea-casa-analysis:2021.07.13"
+    assert dask.config.get('jobqueue.coffea-casa.worker-image') == "coffeateam/coffea-casa-analysis:latest"
