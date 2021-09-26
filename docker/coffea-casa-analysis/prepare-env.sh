5c57de1a3f65b16e342e52f1c5a0ca3b573d1606
Oksana Shadura <ksu.shadura@gmail.com>
2021-09-26 18:29:20 +0200

    Fix a small probable typo

--- docker/coffea-casa-analysis/prepare-env.sh
+++ docker/coffea-casa-analysis/prepare-env.sh
@@ -112,7 +112,7 @@ if [[ ! -v COFFEA_CASA_SIDECAR ]]; then
       --death-timeout 60 \
       --protocol tls \
       --listen-address tls://0.0.0.0:$CONTAINER_PORT \
-      --contact-address tls://$HOST:$PORT
+      --contact-address tls://$HOST:$PORT \
       --nanny-contact-address tls://$HOST:$NANNYCONTAINER_PORT"
       # Debug print
       echo $HTCONDOR_COMAND 1>&2
