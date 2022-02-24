f4d2920c4dd0355afe8a175a670a473c6f4c3393
Andrew42 <awightma@nd.edu>
2022-02-24 12:05:47 -0600

    Add DistributedEnvironmentPlugin

--- coffea_casa/__init__.py
+++ coffea_casa/__init__.py
@@ -5,6 +5,7 @@ import dask
 import yaml
 import os
 from .coffea_casa import CoffeaCasaCluster
+from .plugin import DistributedEnvironmentPlugin
 from . import config
 
 from ._version import get_versions
