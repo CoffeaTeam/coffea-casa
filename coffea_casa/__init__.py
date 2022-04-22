8ed33e3922f17114194e009cdf4cca26d1ea280a
Oksana Shadura <ksu.shadura@gmail.com>
2022-04-22 20:27:06 +0200

    Revert "Add DistributedEnvironmentPlugin"
    This reverts commit f4d2920c4dd0355afe8a175a670a473c6f4c3393.

--- coffea_casa/__init__.py
+++ coffea_casa/__init__.py
@@ -5,7 +5,6 @@ import dask
 import yaml
 import os
 from .coffea_casa import CoffeaCasaCluster
-from .plugin import DistributedEnvironmentPlugin
 from . import config
 
 from ._version import get_versions
