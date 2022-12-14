4bec340287ced07108b8773f04984214f9594b82
Oksana Shadura <ksu.shadura@gmail.com>
2022-12-14 13:44:08 +0100

    Fix test references

--- tests/test_config.py
+++ tests/test_config.py
@@ -4,4 +4,4 @@ import coffea_casa.config
 
 
 def test_dask_config():
-    assert dask.config.get('jobqueue.coffea-casa.worker-image') == "coffeateam/coffea-casa-analysis:latest"
+    assert dask.config.get('jobqueue.coffea-casa.worker-image') == "hub.opensciencegrid.org/coffea-casa/cc-analysis-ubuntu:development"
