1f9be329cb36ac4c3e4b9163095e62b1b52c92a3
Oksana Shadura <ksu.shadura@gmail.com>
2023-02-17 14:17:17 +0100

    Revert "Open 8001 port as one that will be used for nanny-contact-address"
    This reverts commit f1e52765d5b71445b9769a8f5fd4639c65b13f22.

--- coffea_casa/coffea_casa.py
+++ coffea_casa/coffea_casa.py
@@ -11,7 +11,7 @@ from distributed.security import Security
 DEFAULT_SCHEDULER_PORT = 8786
 DEFAULT_DASHBOARD_PORT = 8787
 DEFAULT_CONTAINER_PORT = 8786
-DEFAULT_NANNY_PORT = 8001
+#DEFAULT_NANNY_PORT = 8001
 
 # Security settings for Dask scheduler
 # REMOVE ME (backward compatibity for now)
@@ -71,7 +71,7 @@ class CoffeaCasaCluster(HTCondorCluster):
                  scheduler_options=None,
                  scheduler_port=DEFAULT_SCHEDULER_PORT,
                  dashboard_port=DEFAULT_DASHBOARD_PORT,
-                 nanny_port=DEFAULT_NANNY_PORT,
+                 #nanny_port=DEFAULT_NANNY_PORT,
                  **job_kwargs):
         """
         Parameters
@@ -118,7 +118,7 @@ class CoffeaCasaCluster(HTCondorCluster):
             worker_image=worker_image,
             scheduler_port=scheduler_port,
             dashboard_port=dashboard_port,
-            nanny_port=nanny_port,
+            #nanny_port=nanny_port,
         )
         # Instantiate args and parameters from parent abstract class security=security
         super().__init__(**job_kwargs)
@@ -132,7 +132,7 @@ class CoffeaCasaCluster(HTCondorCluster):
                            scheduler_options=None,
                            scheduler_port=DEFAULT_SCHEDULER_PORT,
                            dashboard_port=DEFAULT_DASHBOARD_PORT,
-                           nanny_port=DEFAULT_NANNY_PORT
+                           #nanny_port=DEFAULT_NANNY_PORT
                            ):
         job_config = job_kwargs.copy()
         input_files = []
@@ -190,9 +190,10 @@ class CoffeaCasaCluster(HTCondorCluster):
                 "docker_image": worker_image or dask.config.get(f"jobqueue.{cls.config_name}.worker-image")
             },
             {
-                "container_service_names": "dask,nanny",
-                "dask_container_port": DEFAULT_CONTAINER_PORT,
-                "nanny_container_port": DEFAULT_NANNY_PORT,
+                #"container_service_names": "dask,nanny",
+                #"dask_container_port": DEFAULT_CONTAINER_PORT,
+                #"nanny_container_port": nanny_port,
+                "container_service_names": "dask",
                 "dask_container_port": DEFAULT_CONTAINER_PORT,
             },
             {"transfer_input_files": files},
