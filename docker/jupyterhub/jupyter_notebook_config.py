7e5b96923b4b909a99a29a1c541e8895a14ba771
Oksana Shadura <ksu.shadura@gmail.com>
2026-06-18 15:26:01 +0200

    Update images according /jupyter/docker-stacks/base-notebook

--- docker/jupyterhub/jupyter_notebook_config.py
+++ docker/jupyterhub/jupyter_notebook_config.py
@@ -1,45 +1,41 @@
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
+# mypy: ignore-errors
+import os
+import stat
+import subprocess
+from pathlib import Path
 
 from jupyter_core.paths import jupyter_data_dir
-import subprocess
-import os
-import errno
-import stat
 
 c = get_config()  # noqa: F821
-c.NotebookApp.ip = "0.0.0.0"
-c.NotebookApp.port = 8888
-c.NotebookApp.open_browser = False
+# Listen on all interfaces (ipv4 and ipv6)
+c.ServerApp.ip = ""
+c.ServerApp.open_browser = False
+
+# to output both image/svg+xml and application/pdf plot formats in the notebook file
+c.InlineBackend.figure_formats = {"png", "jpeg", "svg", "pdf"}
 
 # https://github.com/jupyter/notebook/issues/3130
 c.FileContentsManager.delete_to_trash = False
 
 # Generate a self-signed certificate
-if "GEN_CERT" in os.environ:
-    dir_name = jupyter_data_dir()
-    pem_file = os.path.join(dir_name, "notebook.pem")
-    try:
-        os.makedirs(dir_name)
-    except OSError as exc:  # Python >2.5
-        if exc.errno == errno.EEXIST and os.path.isdir(dir_name):
-            pass
-        else:
-            raise
-
-    # Generate an openssl.cnf file to set the distinguished name
-    cnf_file = os.path.join(os.getenv("CONDA_DIR", "/usr/lib"), "ssl", "openssl.cnf")
-    if not os.path.isfile(cnf_file):
-        with open(cnf_file, "w") as fh:
-            fh.write(
-                """\
+OPENSSL_CONFIG = """\
 [req]
 distinguished_name = req_distinguished_name
 [req_distinguished_name]
 """
-            )
+if "GEN_CERT" in os.environ:
+    dir_name = Path(jupyter_data_dir())
+    dir_name.mkdir(parents=True, exist_ok=True)
+    pem_file = dir_name / "notebook.pem"
 
-    # Generate a certificate if one doesn't exist on disk
+    # Generate an openssl.cnf file to set the distinguished name
+    cnf_file = Path(os.getenv("CONDA_DIR", "/usr/lib")) / "ssl/openssl.cnf"
+    if not cnf_file.exists():
+        cnf_file.write_text(OPENSSL_CONFIG)
+
+    # Generate a certificate if one doesn't exist on a disk
     subprocess.check_call(
         [
             "openssl",
@@ -55,10 +51,9 @@ distinguished_name = req_distinguished_name
         ]
     )
     # Restrict access to the file
-    os.chmod(pem_file, stat.S_IRUSR | stat.S_IWUSR)
-    c.NotebookApp.certfile = pem_file
+    pem_file.chmod(stat.S_IRUSR | stat.S_IWUSR)
+    c.ServerApp.certfile = str(pem_file)
 
-# Change default umask for all subprocesses of the notebook server if set in
-# the environment
+# Change default umask for all subprocesses of the Server if set in the environment
 if "NB_UMASK" in os.environ:
     os.umask(int(os.environ["NB_UMASK"], 8))
--- docker/jupyterhub/start-notebook.sh
+++ docker/jupyterhub/start-notebook.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
-# Copyright (c) Jupyter Development Team.
-# Distributed under the terms of the Modified BSD License.
+# Shim to emit warning and call start-notebook.py
+echo "WARNING: Use start-notebook.py instead"
 
-set -e
-
-wrapper=""
-if [[ "${RESTARTABLE}" == "yes" ]]; then
-    wrapper="run-one-constantly"
-fi
-
-if [[ -n "${JUPYTERHUB_API_TOKEN}" ]]; then
-    # launched by JupyterHub, use single-user entrypoint
-    exec /usr/local/bin/start-singleuser.sh "$@"
-elif [[ -n "${JUPYTER_ENABLE_LAB}" ]]; then
-    # shellcheck disable=SC1091
-    . /usr/local/bin/start.sh ${wrapper} jupyter lab "$@"
-else
-    echo "WARN: Jupyter Notebook deprecation notice https://github.com/jupyter/docker-stacks#jupyter-notebook-deprecation-notice."
-    # shellcheck disable=SC1091
-    . /usr/local/bin/start.sh ${wrapper} jupyter notebook "$@"
-fi
+exec /usr/local/bin/start-notebook.py "$@"
